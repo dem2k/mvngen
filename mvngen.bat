d585af9ee6d99ec0a5402f5ab12cd64759b1fcee
Dimitri Kleyn <dimitri.kleyn@fv.nrw.de>
2017-12-27 12:47:53 +0100

    Remove some DRY Code.

--- mvngen.bat
+++ mvngen.bat
@@ -7,7 +7,7 @@ if not "%_CMDPROC%" == "" (
 )
 
 echo.
-set /p runide="Run IDE and import Preferences and Existing Maven Projects? [Yes] : "
+set /p runide="Run IDE and import Preferences and Existing Maven Projects? [Y/n] : "
 
 set prjname=%1
 echo.
@@ -64,48 +64,70 @@ echo creating logback.xml ...
 copy %~dpn0-logback.xml src\main\resources\logback.xml
 @rem ----------------------------------
 
- >mvn-compile.bat echo @call mvn compile
->>mvn-compile.bat echo @if errorlevel 1 pause
+(
+ echo @call mvn compile
+ echo @if errorlevel 1 pause
+) >mvn-compile.bat 
 
- >mvn-jetty-run.bat echo @SET ROOT=%prjname%
->>mvn-jetty-run.bat echo @SET PORT=8080
->>mvn-jetty-run.bat echo SET MAVEN_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=5%%PORT%%,server=y,suspend=n
->>mvn-jetty-run.bat echo SET MAVEN_CMD=mvn -DskipTests -Djetty.reload=automatic -Djetty.scanIntervalSeconds=5 -Djetty.port=%%PORT%% -Djetty.contextPath=/%%ROOT%% jetty:run
->>mvn-jetty-run.bat echo @TITLE JETTY %%MAVEN_CMD%%
->>mvn-jetty-run.bat echo @START "" http://localhost:%%port%%/%%root%%/application.wadl
->>mvn-jetty-run.bat echo @CALL %%MAVEN_CMD%%
->>mvn-jetty-run.bat echo @IF ERRORLEVEL 1 PAUSE
->>mvn-jetty-run.bat echo @rem -Djetty.scanIntervalSeconds=5
->>mvn-jetty-run.bat echo @rem    The pause in seconds between sweeps of the webapp to check for changes and automatically hot redeploy if any are detected. By default this is 0, which disables hot deployment scanning. A number greater than 0 enables it.
->>mvn-jetty-run.bat echo @rem -Djetty.reload=automatic
->>mvn-jetty-run.bat echo @rem    Default value is "automatic", used in conjunction with a non-zero scanIntervalSeconds causes automatic hot redeploy when changes are detected. Set to "manual" instead to trigger scanning by typing a linefeed in the console running the plugin. This might be useful when you are doing a series of changes that you want to ignore until you're done. In that use, use the reload parameter.
+(
+ echo @SET ROOT=%prjname%
+ echo @SET PORT=8080
+ echo SET MAVEN_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=5%%PORT%%,server=y,suspend=n
+ echo SET MAVEN_CMD=mvn -DskipTests -Djetty.reload=automatic -Djetty.scanIntervalSeconds=5 -Djetty.port=%%PORT%% -Djetty.contextPath=/%%ROOT%% jetty:run
+ echo @TITLE JETTY %%MAVEN_CMD%%
+ echo @START "" http://localhost:%%port%%/%%root%%/application.wadl
+ echo @CALL %%MAVEN_CMD%%
+ echo @IF ERRORLEVEL 1 PAUSE
+ echo @rem -Djetty.scanIntervalSeconds=5
+ echo @rem    The pause in seconds between sweeps of the webapp to check for changes and automatically hot redeploy if any are detected. By default this is 0, which disables hot deployment scanning. A number greater than 0 enables it.
+ echo @rem -Djetty.reload=automatic
+ echo @rem    Default value is "automatic", used in conjunction with a non-zero scanIntervalSeconds causes automatic hot redeploy when changes are detected. Set to "manual" instead to trigger scanning by typing a linefeed in the console running the plugin. This might be useful when you are doing a series of changes that you want to ignore until you're done. In that use, use the reload parameter.
+) >mvn-jetty-run.bat
 
- >mvn-findbugs-gui.bat echo @call mvn findbugs:gui
->>mvn-findbugs-gui.bat echo @if errorlevel 1 pause
+(
+ echo @call mvn findbugs:gui
+ echo @if errorlevel 1 pause
+) >mvn-findbugs-gui.bat
 
- >mvn-dependency-sources-javadoc.bat echo @call mvn dependency:sources dependency:resolve -Dclassifier=javadoc
->>mvn-dependency-sources-javadoc.bat echo @if errorlevel 1 pause
+(
+ echo @call mvn dependency:sources dependency:resolve -Dclassifier=javadoc
+ echo @if errorlevel 1 pause
+) >mvn-dependency-sources-javadoc.bat
 
- >mvn-findbugs-check.bat echo @call mvn compile findbugs:check
->>mvn-findbugs-check.bat echo @if errorlevel 1 pause
+(
+ echo @call mvn compile findbugs:check
+ echo @if errorlevel 1 pause
+) >mvn-findbugs-check.bat
 
- >mvn-copy-dependencies.bat echo @call mvn dependency:copy-dependencies
->>mvn-copy-dependencies.bat echo @if errorlevel 1 pause
+(
+ echo @call mvn dependency:copy-dependencies
+ echo @if errorlevel 1 pause
+) >mvn-copy-dependencies.bat
 
- >mvn-dependency-tree.bat echo @call mvn dependency:tree
->>mvn-dependency-tree.bat echo @if errorlevel 1 pause
+(
+ echo @call mvn dependency:tree
+ echo @if errorlevel 1 pause
+) >mvn-dependency-tree.bat
 
- >mvn-sortpom-verify.bat echo @call mvn com.google.code.sortpom:maven-sortpom-plugin:verify
->>mvn-sortpom-verify.bat echo @if errorlevel 1 pause
+(
+ echo @call mvn com.google.code.sortpom:maven-sortpom-plugin:verify
+ echo @if errorlevel 1 pause
+) >mvn-sortpom-verify.bat 
 
- >mvn-eclipse-eclipse.bat echo @call mvn eclipse:eclipse
->>mvn-eclipse-eclipse.bat echo @if errorlevel 1 pause
+(
+ echo @call mvn eclipse:eclipse
+ echo @if errorlevel 1 pause
+) >mvn-eclipse-eclipse.bat 
 
- >mvn-findbugs-gui.bat echo @call mvn findbugs:gui
->>mvn-findbugs-gui.bat echo @if errorlevel 1 pause
+(
+ echo @call mvn findbugs:gui
+ echo @if errorlevel 1 pause
+) >mvn-findbugs-gui.bat
 
- >mvn-eclipse-configure-workspace.bat echo @mvn eclipse:configure-workspace -Declipse.workspace=c:\xtest
->>mvn-eclipse-configure-workspace.bat echo @if errorlevel 1 pause
+(
+ echo @mvn eclipse:configure-workspace -Declipse.workspace=c:\xtest
+ echo @if errorlevel 1 pause
+) >mvn-eclipse-configure-workspace.bat 
 
  >class-runner.bat echo @echo off
 >>class-runner.bat echo set CLASSPATH=target\classes;config
@@ -116,23 +138,29 @@ copy %~dpn0-logback.xml src\main\resources\logback.xml
 >>class-runner.bat echo set CLASSPATH=%%1;%%CLASSPATH%%
 >>class-runner.bat echo :ende
 
- >runaclass.bat echo @call %%~dp0class-runner.bat %%~n0 %%*
->>runaclass.bat echo @if errorlevel 1 pause
+(
+ echo @call %%~dp0class-runner.bat %%~n0 %%*
+ echo @if errorlevel 1 pause
+) >runaclass.bat 
 
- >mvn-exec-java.bat echo @call mvn exec:java -Dexec.mainClass="d2k.App" -Dexec.args="argument1" -Dexec.args="argument2"
->>mvn-exec-java.bat echo @if errorlevel 1 pause
+(
+ echo @call mvn exec:java -Dexec.mainClass="d2k.App" -Dexec.args="argument1" -Dexec.args="argument2"
+ echo @if errorlevel 1 pause
+) >mvn-exec-java.bat 
 
- >mvn-surefire-report.bat echo @echo off
->>mvn-surefire-report.bat echo set report=
->>mvn-surefire-report.bat echo set /p report=Run tests (ENTER=No, something else=Yes)? 
->>mvn-surefire-report.bat echo if "%%report%%" == "" goto ronly
->>mvn-surefire-report.bat echo call mvn surefire-report:report
->>mvn-surefire-report.bat echo goto finish
->>mvn-surefire-report.bat echo :ronly
->>mvn-surefire-report.bat echo call mvn surefire-report:report-only
->>mvn-surefire-report.bat echo :finish
->>mvn-surefire-report.bat echo if errorlevel 1 pause
->>mvn-surefire-report.bat echo start target/site/surefire-report.html
+(
+ echo @echo off
+ echo set report=
+ echo set /p report="Run tests? [y/N] : "
+ echo if "%%report%%" == "" goto ronly
+ echo call mvn surefire-report:report
+ echo goto finish
+ echo :ronly
+ echo call mvn surefire-report:report-only
+ echo :finish
+ echo if errorlevel 1 pause
+ echo start target/site/surefire-report.html
+) >mvn-surefire-report.bat
 
 
 (
@@ -145,17 +173,19 @@ copy %~dpn0-logback.xml src\main\resources\logback.xml
  echo "%%COMMANDER_EXE%%" /O /S "%%DRV%%"
 ) >map-drive.bat
 
- >map-drive-for-%prjname%.bat echo SET PRJDRV=N
->>map-drive-for-%prjname%.bat echo IF EXIST %%PRJDRV%%:\%%~NX0 GOTO finish
->>map-drive-for-%prjname%.bat echo %%~D0
->>map-drive-for-%prjname%.bat echo CD "%%~DP0"
->>map-drive-for-%prjname%.bat echo SUBST %%PRJDRV%%: /D
->>map-drive-for-%prjname%.bat echo SUBST %%PRJDRV%%: .
->>map-drive-for-%prjname%.bat echo IF ERRORLEVEL 1 PAUSE
->>map-drive-for-%prjname%.bat echo :finish
->>map-drive-for-%prjname%.bat echo %%PRJDRV%%:
->>map-drive-for-%prjname%.bat echo CD %%PRJDRV%%:\
->>map-drive-for-%prjname%.bat echo "%%COMMANDER_EXE%%" /O /S "%%PRJDRV%%:\"
+(
+ echo SET PRJDRV=N
+ echo IF EXIST %%PRJDRV%%:\%%~NX0 GOTO finish
+ echo %%~D0
+ echo CD "%%~DP0"
+ echo SUBST %%PRJDRV%%: /D
+ echo SUBST %%PRJDRV%%: .
+ echo IF ERRORLEVEL 1 PAUSE
+ echo :finish
+ echo %%PRJDRV%%:
+ echo CD %%PRJDRV%%:\
+ echo "%%COMMANDER_EXE%%" /O /S "%%PRJDRV%%:\"
+) >map-drive-for-%prjname%.bat 
 
 
 echo.
